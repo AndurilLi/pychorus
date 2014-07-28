feaef4be15a2a5f5bd3b5351e726b82b3edf1f13
yue <yue@Yues-Mac-Air.local>
2014-07-27 23:00:42 -0400

    Add 'before_run' and 'after_run' in Sample

---
+++ ChorusCore/Samples/TestSuite/ProjectRun.py
@@ -0,0 +1,12 @@
+'''
+Created on Jul 27, 2014
+
+@author: yue
+'''
+#before_run will be executed before all test suites.
+def before_run():
+    print 'before_run'
+
+#after_run will be executed after all test suites.
+def after_run():
+    print 'after_run'
